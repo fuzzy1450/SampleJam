4c6406fb62c0453439acb4e200e22b24f2bdad93
fuzzy1450 <ievank@me.com>
2016-02-14 03:16:58 -0500

    finally color manipulator? and temporary color input

--- keyboardManipulator.js
+++ keyboardManipulator.js
@@ -24,39 +24,31 @@ function recolorKeyboard(color) {
 	}
 }
 
-function tintImage(imgElement,tintColor) {
-    // create hidden canvas (using image dimensions)
-    var canvas = document.createElement("canvas");
-    canvas.width = imgElement.offsetWidth;
-    canvas.height = imgElement.offsetHeight;
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(imgElement,0,0);
-
-    var map = ctx.getImageData(0,0,320,240);
-    var imdata = map.data;
-
-    // convert image to grayscale
-    var r,g,b,avg;
-    for(var p = 0, len = imdata.length; p < len; p+=4) {
-        r = imdata[p]
-        g = imdata[p+1];
-        b = imdata[p+2];
-        // alpha channel (p+3) is ignored           
-
-        avg = Math.floor((r+g+b)/3);
-
-        imdata[p] = imdata[p+1] = imdata[p+2] = avg;
-    }
-
-    ctx.putImageData(map,0,0);
-
-    // overlay filled rectangle using lighter composition
-    ctx.globalCompositeOperation = "lighter";
-    ctx.globalAlpha = 0.5;
-    ctx.fillStyle=tintColor;
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-
-    // replace image source with canvas data
-    imgElement.src = canvas.toDataURL();
+function hexToRGB(hex)
+{
+    var long = parseInt(hex.replace(/^#/, ""), 16);
+    return {
+        R: (long >>> 16) & 0xff,
+        G: (long >>> 8) & 0xff,
+        B: long & 0xff
+    };
 }
+
+ function changeColor()
+    {
+        if(!originalPixels) return; // Check if image has loaded
+        var newColor = hexToRGB(document.getElementById("color").value);
+
+        for(var I = 0, L = originalPixels.data.length; I < L; I += 4)
+        {
+            if(currentPixels.data[I + 3] > 0) // If it's not a transparent pixel
+            {
+                currentPixels.data[I] = originalPixels.data[I] / 255 * newColor.R;
+                currentPixels.data[I + 1] = originalPixels.data[I + 1] / 255 * newColor.G;
+                currentPixels.data[I + 2] = originalPixels.data[I + 2] / 255 * newColor.B;
+            }
+        }
+
+        ctx.putImageData(currentPixels, 0, 0);
+        mug.src = canvas.toDataURL("image/png");
+    }
