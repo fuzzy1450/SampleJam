c32c245265ec238f5c0eabde250aa245f75d84a3
fuzzy1450 <ievank@me.com>
2016-02-14 02:06:26 -0500

    ayy mlao

--- keyboardManipulator.js
+++ keyboardManipulator.js
@@ -1,8 +1,16 @@
 canvases=document.getElementsByClassName("canvas");
 imageNames = ["1","2","3","4","5","6","7","8","9","0","Q2","W2","E2","R2","T2","Y2","U2","I2","O2","P2","A2","S2","D2","F2","G2","H2","J2","K2","L2","Z2","X2","C2","V2","B2","N2","M2"]
 
-
-for(xe=0; xe!=canvases.length; xe++){
-	myImage = new Image();
-	myImage.src = ('./assets/keyboard/'+imageNames[xe]);
+function startKeyboard(){
+	for(xe=0; xe!=canvases.length; xe++){
+		var canvas = canvases[xe];
+		ctx = canvas.getContext("2d");
+	
+	
+		img = new Image(600, 600);
+		img.src = ('./assets/keyboard/'+imageNames[xe]+".png");
+	
+		ctx.drawImage(img,0,0);
+	
+	}
 }
