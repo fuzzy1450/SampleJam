ca71b92e2dffcd7167758f5baa58964c7c67656f
fuzzy1450 <ievank@me.com>
2016-02-14 03:01:34 -0500

    wat
    fgdewujyi

--- keyboardManipulator.js
+++ keyboardManipulator.js
@@ -16,3 +16,47 @@ function startKeyboard(){
 	
 	}
 }
+
+function recolorKeyboard(color) {
+	for(xe=0; xe!=canvases.length; xe++){
+		var canvas = canvases[xe];
+		tintImage(canvas, color);
+	}
+}
+
+function tintImage(imgElement,tintColor) {
+    // create hidden canvas (using image dimensions)
+    var canvas = document.createElement("canvas");
+    canvas.width = imgElement.offsetWidth;
+    canvas.height = imgElement.offsetHeight;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(imgElement,0,0);
+
+    var map = ctx.getImageData(0,0,320,240);
+    var imdata = map.data;
+
+    // convert image to grayscale
+    var r,g,b,avg;
+    for(var p = 0, len = imdata.length; p < len; p+=4) {
+        r = imdata[p]
+        g = imdata[p+1];
+        b = imdata[p+2];
+        // alpha channel (p+3) is ignored           
+
+        avg = Math.floor((r+g+b)/3);
+
+        imdata[p] = imdata[p+1] = imdata[p+2] = avg;
+    }
+
+    ctx.putImageData(map,0,0);
+
+    // overlay filled rectangle using lighter composition
+    ctx.globalCompositeOperation = "lighter";
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle=tintColor;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    // replace image source with canvas data
+    imgElement.src = canvas.toDataURL();
+}
